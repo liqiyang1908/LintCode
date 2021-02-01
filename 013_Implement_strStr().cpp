b5a2dd7c6de17d2f8690d59532f146eb84727a3b
liqiyang1908 <liqiyang1908@users.noreply.github.com>
2021-01-31 18:25:18 -0800

    Update 013_Implement_strStr().cpp

--- 013_Implement_strStr().cpp
+++ 013_Implement_strStr().cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
-    /*
-     * @param source: source string to be scanned.
-     * @param target: target string containing the sequence of characters to match
-     * @return: a index to the first occurrence of target in source, or -1  if target is not part of source.
+
+    /**
+     * @param source: 
+     * @param target: 
+     * @return: return the index
      */
-    int strStr(const char *source, const char *target) {
-        if(source == nullptr || target == nullptr){
-            return -1;
-        }
-        string s1 = source;
-        string t1 = target;
-        if ( s1.size()==0 && t1.size()==0){
+
+    int strStr(string &source, string &target) {
+        //获取两个字符串的长度
+        int sourceLen = source.size();
+        int targetLen = target.size();
+
+        //注意特判，若target长度为0则答案为0
+        if(targetLen == 0){
             return 0;
         }
-        
-        for(unsigned i=0; i<(s1.size() - t1.size()+1); i++){
-            bool foundMisMatch = false;
-            for(unsigned j=0; j< t1.size(); j++){
-                if(s1[i+j] != t1[j]){
-                    foundMisMatch = true;
+
+        //若target长度大于source，则不可能匹配
+        if(targetLen > sourceLen){
+            return -1;
+        }
+
+        for(int i=0; i < sourceLen-targetLen+1; i++){
+            //k表示比对时source中所指向的字符
+            int k = i;
+            for(int j = 0; j < targetLen; j++){
+                if(source[k] == target[j]){
+                    //最后一个字符匹配完成，输出答案
+                    if(j == targetLen - 1){
+                        return i;
+                    }
+                    k++;
+                }
+                //其中一个字符无法匹配，直接退出做下一次循环
+                else{
                     break;
                 }
             }
-            if(!foundMisMatch){
-                return i;
-            }
         }
-            
         return -1;
     }
 };
