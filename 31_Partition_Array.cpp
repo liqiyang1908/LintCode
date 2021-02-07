3ea4970e5ce82babea77ce9ab33d0da99b7b5368
liqiyang1908 <liqiyang1908@users.noreply.github.com>
2021-02-06 16:51:19 -0800

    Add files via upload

---
+++ 31_Partition_Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    /**
+     * @param nums: The integer array you should partition
+     * @param k: An integer
+     * @return: The index after partition
+     */
+    int partitionArray(vector<int>& nums, int k) {
+        if (nums.size() < 1)
+            return 0;
+        int l = 0, r = nums.size() - 1;
+        while (l <= r)
+        {
+            while (l <= r && nums[l] < k)
+                l++;
+            while (l <= r && nums[r] >= k)
+                r--;
+            if (l <= r)
+                swap(nums[l++], nums[r--]);
+        }
+        return l;
+    }
+};
