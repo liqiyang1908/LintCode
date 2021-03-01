a2d3ddcf5e769349878e27c8383d71b9fa66a220
liqiyang1908 <liqiyang1908@users.noreply.github.com>
2021-02-28 19:56:29 -0800

    Update 135_Combination_Sum.cpp

--- 135_Combination_Sum.cpp
+++ 135_Combination_Sum.cpp
@@ -1,3 +1,4 @@
+//  method 1
 class Solution {
 public:
     /**
@@ -25,6 +26,46 @@ public:
         vector<vector<int>>res;
         sort(candidates.begin(), candidates.end());
         dfs(candidates, 0, target, path, 0, res);
+        return res;
+    }
+};
+
+//method 2
+class Solution {
+public:
+    /**
+     * @param candidates: A list of integers
+     * @param target: An integer
+     * @return: A list of lists of integers
+     */
+    void dfs(vector<int>& candidates, int startidx, int target, int sum, vector<int>& path, vector<vector<int>>& res)
+    {
+        if (sum == target)
+            res.push_back(path);
+        if (sum > target)
+            return;
+        for (int i = startidx; i < candidates.size(); i++)
+        {
+            path.push_back(candidates[i]);
+            dfs(candidates, i, target, sum + candidates[i], path, res);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        unordered_set<int>dup;
+        vector<int>nums;
+        for (auto it : candidates)
+        {
+            if (dup.find(it) == dup.end())
+            {
+                nums.push_back(it);
+                dup.insert(it);
+            }
+        }
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>res;
+        vector<int>path;
+        dfs(nums, 0, target, 0, path, res);
         return res;
     }
 };
