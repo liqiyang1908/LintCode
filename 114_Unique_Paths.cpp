5a6b30e89cf6702db6c4d77bfba7a54383a5e2e2
liqiyang1908 <liqiyang1908@users.noreply.github.com>
2021-03-06 21:44:33 -0800

    Add files via upload

---
+++ 114_Unique_Paths.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    /**
+     * @param m: positive integer (1 <= m <= 100)
+     * @param n: positive integer (1 <= n <= 100)
+     * @return: An integer
+     */
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++)
+        {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
