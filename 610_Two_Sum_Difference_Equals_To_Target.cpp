68cd6f73531a6f1af28c0cd4bb3ca4852344230f
liqiyang1908 <liqiyang1908@users.noreply.github.com>
2021-03-08 22:11:31 -0800

    Update 610_Two_Sum_Difference_Equals_To_Target.cpp

--- 610_Two_Sum_Difference_Equals_To_Target.cpp
+++ 610_Two_Sum_Difference_Equals_To_Target.cpp
@@ -27,3 +27,37 @@ public:
         return res;
     }
 };
+
+
+//同向双指针
+class Solution {
+public:
+    /**
+     * @param nums: an array of Integer
+     * @param target: an integer
+     * @return: [num1, num2] (num1 < num2)
+     */
+    vector<int> twoSum7(vector<int>& nums, int target) {
+        vector<int>res;
+        if (nums.size() < 2)
+            return res;
+        int i, j = 1;
+        if (target < 0)
+            target = 0 - target;
+        for (i = 0; i < nums.size(); i++)
+        {
+            j = max(i + 1, j);
+            while (j < nums.size() && nums[j] - nums[i] < target)
+                j++;
+            if (j >= nums.size())
+                break;
+            if (nums[j] - nums[i] == target)
+            {
+                res.push_back(nums[i]);
+                res.push_back(nums[j]);
+                return res;
+            }
+        }
+        return res;
+    }
+};
