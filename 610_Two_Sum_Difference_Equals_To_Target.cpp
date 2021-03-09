9fa4612a4c30dd80b563348769ad40702f8c6c5d
liqiyang1908 <liqiyang1908@users.noreply.github.com>
2021-03-08 20:01:35 -0800

    Add files via upload

---
+++ 610_Two_Sum_Difference_Equals_To_Target.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    /**
+     * @param nums: an array of Integer
+     * @param target: an integer
+     * @return: [num1, num2] (num1 < num2)
+     */
+    vector<int> twoSum7(vector<int>& nums, int target) {
+        vector<int>res;
+        if (nums.size() < 2)
+            return res;
+        unordered_set<int>mymap;
+        if (target < 0)
+            target = 0 - target;
+        for (auto num: nums)
+        {
+            int first_num = num - target;
+            if (mymap.find(first_num) != mymap.end())
+            {
+                res.push_back(first_num);
+                res.push_back(num);
+                return res;
+            }
+            if (mymap.find(num) == mymap.end())
+                mymap.insert(num);
+        }
+        return res;
+    }
+};
