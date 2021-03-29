5b316945cdfce31043aaf70859c1800264afd433
liqiyang1908 <liqiyang1908@users.noreply.github.com>
2021-03-29 15:02:55 -0700

    Update 92_Backpack.cpp

--- 92_Backpack.cpp
+++ 92_Backpack.cpp
@@ -1,3 +1,6 @@
+//Method 1
+
+
 class Solution {
 public:
     /**
@@ -7,7 +10,7 @@ public:
      */
     int backPack(int m, vector<int>& A) {
         int n = A.size();
-        vector<vector<int>>dp(n + 1, vector<int>(m + 1, false));
+        vector<vector<bool>>dp(n + 1, vector<bool>(m + 1, false));
         dp[0][0] = true;
         for (int i = 1; i <= n; i++)
         {
@@ -20,11 +23,39 @@ public:
                     dp[i][j] = dp[i - 1][j];
             }
         }
-        for (int i = m; i >= 0; i--)
+        for (int i = m; i > 0; i--)
         {
             if (dp[n][i])
                 return i;
         }
         return -1;
+    }
+};
+
+
+//Method 2
+
+class Solution {
+public:
+    /**
+     * @param m: An integer m denotes the size of a backpack
+     * @param A: Given n items with size A[i]
+     * @return: The maximum size
+     */
+    int backPack(int m, vector<int>& A) {
+        int n = A.size();
+        vector<vector<int>>dp(n + 1, vector<int>(m + 1, 0));
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j <= m; j++)
+            {
+                if (j >= A[i - 1])
+                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - A[i - 1]] + A[i-1]);
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][m];
     }
 };
