3ef0f607e05e92c148f756f4666842e19af677da
liqiyang1908 <liqiyang1908@users.noreply.github.com>
2021-04-10 17:21:09 -0700

    Update 539_Move_Zeroes.cpp

--- 539_Move_Zeroes.cpp
+++ 539_Move_Zeroes.cpp
@@ -16,3 +16,25 @@ public:
         }
     }
 };
+
+
+//Method 2
+class Solution {
+public:
+    /**
+     * @param nums: an integer array
+     * @return: nothing
+     */
+    void moveZeroes(vector<int>& nums) {
+        int left = 0;
+        for (int right = 0; right < nums.size(); right++)
+        {
+            if (nums[right] != 0)
+            {
+                nums[left++] = nums[right];
+            }
+        }
+        while (left < nums.size())
+            nums[left++] = 0;
+    }
+};
